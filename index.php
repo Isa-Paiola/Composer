f0806143dd8cfec8583933e61891c1bace809e01
Isa-Paiola <isabelapaiola6@gmail.com>
2025-04-01 11:47:20 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+// autoload do composer
+
+$faker = Faker\Factory::create();
+// criando um gerador de dados
+
+echo "Nome:" . $faker->name() . "<br> <br>";
+echo "Email:" . $faker->email() . "<br> <br>";
+echo "Endereço:" . $faker->address() . "<br> <br>";
+
+?>
